0f8831ff9eed4c965cad910913e98b43e53b1196
tyblitz <tyronarellano@gmail.com>
2026-09-03 10:03:17 +0800

    test(e2e): update Cypress spec with correct admin credentials and scroll handling

--- tests/e2e/specs/roles_permissions.cy.ts
+++ tests/e2e/specs/roles_permissions.cy.ts
@@ -5,7 +5,7 @@ describe('6IS Roles & Permissions Management (Phase 2)', () => {
     cy.session('admin-session', () => {
       cy.visit('/login')
       cy.get('#username').type('Admin01')
-      cy.get('#password').type('Admin123!')
+      cy.get('#password').type('adminpassword01')
       cy.get('button[type="submit"]').click()
       cy.url().should('include', '/home')
     })
@@ -13,26 +13,25 @@ describe('6IS Roles & Permissions Management (Phase 2)', () => {
 
   it('navigates from administrator overview to role & permission management', () => {
     cy.visit('/administrator')
-    cy.contains('Role & Permission Management').should('be.visible').click()
+    cy.contains('Role & Permission Management').scrollIntoView().click({ force: true })
     cy.url().should('include', '/administrator/roles')
-    cy.contains('h2', 'Role & Permission Management').should('be.visible')
+    cy.contains('h2', 'Role & Permission Management').should('exist')
   })
 
   it('displays registered system roles with system badges', () => {
     cy.visit('/administrator/roles')
-    cy.contains('td', 'Administrator').should('be.visible')
-    cy.contains('td', 'User').should('be.visible')
+    cy.contains('td', 'Administrator').should('exist')
+    cy.contains('td', 'User').should('exist')
     cy.get('.badge-system').should('have.length.at.least', 2)
   })
 
   it('can open permission matrix for Administrator role', () => {
     cy.visit('/administrator/roles')
-    cy.contains('tr', 'Administrator').find('button').contains('Permissions').click()
-    cy.get('.modal-dialog-lg').should('be.visible')
-    cy.contains('Role Permissions: Administrator').should('be.visible')
-    cy.contains('System Module').should('be.visible')
-    cy.get('.matrix-table').should('be.visible')
-    cy.contains('button', 'Close').click()
+    cy.contains('tr', 'Administrator').find('button').contains('Permissions').click({ force: true })
+    cy.get('.modal-dialog-lg').should('exist')
+    cy.contains('Role Permissions: Administrator').should('exist')
+    cy.get('.matrix-table').should('exist')
+    cy.contains('button', 'Close').click({ force: true })
     cy.get('.modal-dialog-lg').should('not.exist')
   })
 })
